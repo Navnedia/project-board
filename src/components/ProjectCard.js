9c70292265d9214d608004b21557a481ea6e2b94
Navnedia <87249556+Navnedia@users.noreply.github.com>
2024-05-26 06:32:18 +0000

    Built project card component

---
+++ src/components/ProjectCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Ratio from 'react-bootstrap/Ratio';
+import Card from 'react-bootstrap/Card';
+// import Button from 'react-bootstrap/Button';
+import '../styles/ProjectCard.css';
+
+function ProjectCard({project, ...props}) {
+    return (
+        <Card className="p-2" bg="dark" text="light">
+            <Ratio aspectRatio="4x3">
+                <Card.Img src={project.coverImageURL} 
+                          variant="top" 
+                          className="rounded" 
+                          style={{ "object-fit": "cover" }} 
+                />
+            </Ratio>
+
+            <Card.Body>
+                <Card.Title>{project.title}</Card.Title>
+                <Card.Text className='line-limit-4'>{project.summary}</Card.Text>
+                
+                {/* <div className="d-flex justify-content-end">
+                    <Button href="" className="stretched-link">
+                        View Project
+                    </Button>
+                </div> */}
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default ProjectCard;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+// import App from './App';
+import ProjectCard from './components/ProjectCard';
 import reportWebVitals from './reportWebVitals';
+
+const projectDetails = {
+    id: 1,
+    title: "Project Title",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Accumsan in nisl nisi scelerisque eu ultrices. Amet consectetur adipiscing elit ut aliquam purus sit amet. Donec pretium vulputate sapien nec sagittis. Eget lorem dolor sed viverra.",
+    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet cursus sit amet dictum sit.",
+    coverImageURL: "https://placehold.co/1280x960",
+    contactEmail: "address@example.com"
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        {/* <App /> */}
+        <ProjectCard project={projectDetails} />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
